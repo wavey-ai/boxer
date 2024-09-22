3c9a1e02b4482557e611955b55633365f8bec6c8
jbrough <jamie1612@gmail.com>
2024-09-22 02:32:07 +0100

    wip: flac

--- src/fmp4.rs
+++ src/fmp4.rs
@@ -1,6 +1,6 @@
 use access_unit::aac::extract_aac_data;
 use access_unit::flac::{create_streaminfo, decode_frame_header};
-use access_unit::Fmp4;
+use access_unit::{detect_audio, Fmp4};
 use access_unit::{AccessUnit, AudioType};
 use bytes::Bytes;
 use mse_fmp4::io::WriteTo;
@@ -39,7 +39,6 @@ pub fn box_fmp4(
     avcc: Option<&AvcDecoderConfigurationRecord>,
     avcs: Vec<AccessUnit>,
     audio_units: Vec<AccessUnit>,
-    audio_type: AudioType,
     last_dts: u64,
 ) -> Fmp4 {
     let mut segment = MediaSegment::new(seq);
@@ -48,7 +47,7 @@ pub fn box_fmp4(
     let mut total_ticks = 0;
     let mut is_key = false;
     let mut avc_data = Vec::new();
-    let mut audio_data = Vec::new();
+    let mut audio_data: Vec<u8> = Vec::new();
 
     let mut avc_samples = Vec::new();
     let mut audio_samples = Vec::new();
@@ -118,7 +117,17 @@ pub fn box_fmp4(
 
     let mut audio_track = TrackBox::new(1, false);
     let mut frame_info = None;
+
+    let mut audio_type = AudioType::Unkownn;
+
+    let audio_type = if audio_units.len() > 0 {
+        detect_audio(&audio_units[0].data)
+    } else {
+        AudioType::Unkownn
+    };
+
     match audio_type {
+        AudioType::Unkownn => {}
         AudioType::FLAC => {
             let info = decode_frame_header(&audio_units[0].data).unwrap();
             let frame_duration = info.block_size;
@@ -147,6 +156,9 @@ pub fn box_fmp4(
             audio_track.tkhd_box.duration = 0;
             audio_track.mdia_box.mdhd_box.timescale = 1000;
             audio_track.mdia_box.mdhd_box.duration = 0;
+
+            segment.update_offsets();
+            segment.write_to(&mut fmp4_data).unwrap();
         }
         AudioType::AAC => {
             let mut sampling_frequency =
@@ -212,12 +224,12 @@ pub fn box_fmp4(
                     .sample_entries
                     .push(SampleEntry::Aac(aac_sample_entry));
             }
+
+            segment.update_offsets();
+            segment.write_to(&mut fmp4_data).unwrap();
         }
         _ => {}
     }
-    segment.update_offsets();
-    segment.write_to(&mut fmp4_data).unwrap();
-
     // create init.mp4
     let mut segment = InitializationSegment::default();
     segment.moov_box.mvhd_box.timescale = 48000;
@@ -318,207 +330,5 @@ pub fn box_fmp4(
         duration: ticks_to_ms(total_ticks) as u32,
         key: is_key,
         data: Bytes::from(fmp4_data),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use access_unit::flac::split_flac_frames;
-    use hyper_hls::HyperHls;
-    use soundkit::audio_bytes::{f32le_to_s24, s16le_to_i32, s24le_to_i32};
-    use soundkit::audio_packet::Encoder;
-    use soundkit::wav::WavStreamProcessor;
-    use soundkit_flac::FlacEncoder;
-    use std::env;
-    use std::fs::File;
-    use std::io::Read;
-    use std::io::Write;
-    use std::sync::Arc;
-    use tokio;
-    use ts::playlist;
-
-    fn is_valid_24bit_sample(sample: i32) -> bool {
-        sample >= -8_388_608 && sample <= 8_388_607
-    }
-
-    async fn run_flac_encoder_with_wav_file(file_path: &str) {
-        const ENV_FILE: &str = include_str!("../.env");
-
-        for line in ENV_FILE.lines() {
-            if let Some((key, value)) = line.split_once('=') {
-                env::set_var(key.trim(), value.trim());
-            }
-        }
-
-        let frame_len = 4096;
-        let mut file = File::open(file_path).unwrap();
-        let mut file_buffer = Vec::new();
-        file.read_to_end(&mut file_buffer).unwrap();
-
-        let mut processor = WavStreamProcessor::new();
-        let audio_data = processor.add(&file_buffer).unwrap().unwrap();
-
-        dbg!(file_path, audio_data.sampling_rate());
-
-        let mut encoder = FlacEncoder::new(
-            audio_data.sampling_rate(),
-            audio_data.bits_per_sample() as u32,
-            audio_data.channel_count() as u32,
-            0 as u32,
-            5,
-        );
-
-        dbg!(audio_data.bits_per_sample());
-        encoder.init().expect("Failed to initialize FLAC encoder");
-
-        let i32_samples = match audio_data.bits_per_sample() {
-            16 => {
-                // this doesn't scale the 16 bit samples - important!
-                s16le_to_i32(audio_data.data())
-            }
-            24 => s24le_to_i32(audio_data.data()),
-            32 => f32le_to_s24(audio_data.data()),
-            _ => {
-                vec![0i32]
-            }
-        };
-
-        let chunk_size = frame_len * audio_data.channel_count() as usize;
-
-        let mut fmp4_segments = Vec::new();
-        let mut init_segment = None;
-        let mut frames = Vec::new();
-
-        let sample_rate = audio_data.sampling_rate() as u64;
-        let channels = audio_data.channel_count() as u64;
-        let samples_per_chunk = chunk_size as u64 / channels;
-        let time_base = 1_000_000; // Use microseconds for precision
-
-        let (playlists, fmp4_cache, m3u8_cache) =
-            playlist::Playlists::new(playlist::Options::default());
-
-        let mut seq = 0;
-        let mut last_dts = 0;
-        let mut accumulated_au: Vec<AccessUnit> = Vec::new();
-        let mut accumulated_duration: u64 = 0;
-        const TARGET_DURATION_MS: u64 = 300;
-
-        for (i, chunk) in i32_samples.chunks(chunk_size).enumerate() {
-            let mut output_buffer = vec![0u8; chunk.len() * std::mem::size_of::<i32>() * 3];
-
-            for sample in chunk {
-                if !is_valid_24bit_sample(*sample) {
-                    panic!("{}", sample);
-                }
-            }
-
-            match encoder.encode_i32(chunk, &mut output_buffer) {
-                Ok(encoded_len) => {
-                    dbg!(encoded_len);
-                    if encoded_len > 0 {
-                        let data = &output_buffer[..encoded_len];
-                        let chunk_duration = samples_per_chunk * time_base / sample_rate;
-                        let pts = i as u64 * chunk_duration;
-                        let dts = pts;
-
-                        frames.push(data.to_owned());
-
-                        let au = AccessUnit {
-                            dts,
-                            pts,
-                            key: true,
-                            avc: false,
-                            data: Bytes::from(data.to_vec()),
-                        };
-
-                        accumulated_au.push(au);
-                        accumulated_duration += chunk_duration;
-
-                        if accumulated_duration * 1000 / time_base >= TARGET_DURATION_MS {
-                            seq += 1;
-                            let duration_ms: u64 = accumulated_duration * 1000 / time_base;
-
-                            let mut fmp4 = box_fmp4(
-                                seq,
-                                None,
-                                Vec::new(),
-                                accumulated_au.clone(),
-                                AudioType::FLAC,
-                                last_dts,
-                            );
-
-                            last_dts = dts;
-                            fmp4.duration = duration_ms as u32;
-                            fmp4_segments.push(fmp4.data.clone());
-
-                            if init_segment.is_none() {
-                                init_segment = fmp4.init.clone();
-                            }
-
-                            playlists.add(1, fmp4);
-
-                            // Reset accumulated data
-                            accumulated_au.clear();
-                            accumulated_duration = 0;
-                        }
-                    }
-                }
-                Err(e) => {
-                    panic!("Failed to encode chunk {}: {:?}", i, e);
-                }
-            }
-        }
-        // Write FMP4 file
-        let output_file_path = format!("{}.fmp4", file_path);
-        let mut output_file = File::create(output_file_path).expect("Failed to create output file");
-
-        let flac_output_file_path = format!("{}.flac", file_path);
-        let mut flac_output_file =
-            File::create(flac_output_file_path).expect("Failed to create output file");
-
-        if let Some(init) = init_segment {
-            output_file
-                .write_all(&init)
-                .expect("Failed to write init segment");
-        };
-
-        // Write data segments
-        for segment in fmp4_segments {
-            output_file
-                .write_all(&segment)
-                .expect("Failed to write data segment");
-        }
-
-        for frame in frames {
-            flac_output_file
-                .write_all(&frame)
-                .expect("Failed to write data segment");
-        }
-
-        println!("FMP4 file written successfully");
-
-        let server = HyperHls::new(
-            env::var_os("CERT_PEM")
-                .expect("CERT_PEM not set")
-                .into_string()
-                .expect("CERT_PEM contains invalid Unicode"),
-            env::var_os("PRIVKEY_PEM")
-                .expect("CERT_PEM not set")
-                .into_string()
-                .expect("CERT_PEM contains invalid Unicode"),
-            4433,
-            Arc::clone(&fmp4_cache),
-            Arc::clone(&m3u8_cache),
-        );
-
-        let (hls_up, hls_fin, hls_shutdown) = server.start().await.unwrap();
-
-        hls_fin.await;
-    }
-
-    #[tokio::test]
-    async fn test_flac_encoder_with_wave_lori() {
-        run_flac_encoder_with_wav_file("testdata/lori.wav").await;
     }
 }
